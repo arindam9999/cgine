180b747c69cf5872dcb1ac5138957ffac2442bbf
Apple <apple@Apples-MacBook-Air.local>
2024-06-02 01:17:40 +0530

    Implement __king_check and __piece_in_path

--- chess.py
+++ chess.py
@@ -161,13 +161,96 @@ class Pawn(Piece):
 
 
 class Board:
-    def __king_check(self):
-        # TODO 
-        pass
+    def is_valid_matrix_location(self, X, Y):
+        for val in [X, Y]:
+            if val < 0 or val >= 8:
+                return False 
+        return True
+
+    def __piece_in_path(self, move):
+        ix, iy, fx, fy = move.initial_pos[0], move.initial_pos[1], move.final_pos[0], move.final_pos[1]
+        dx, dy = fx - ix, fy - iy 
+        x, y = ix, iy 
+        while x != fx or y != fy:
+            if self.matrix[x][y] != None:
+                return False 
+            x, y = x + dx, y + dy 
+        return True
+
+    def __is_check_from_knight(self, matrix, king_location):
+        color = self.to_move 
+        dy = [1, 2, 2, 1, -1, -2, -2, -1]
+        dx = [2, 1, -1, -2, -2, -1, 1, 2]
+        x, y = king_location[color]
+        for i in range(0, 8):
+            X, Y = x + dx[i], y + dy[i] 
+            # Case we X, Y not part of matrix
+            if not self.is_valid_matrix_location(X, Y):
+                continue 
+            # Case where no piece in the current location
+            if matrix[X][Y] == None:
+                continue
+            if (matrix[X][Y].piece_type == "N") and (matrix[X][Y].color != matrxi[x][y].color):
+                return True 
+        return False 
     
-    def __piece_in_path(self):
-        # TODO
-        pass
+    def __is_row_column_diagonal_check(self, matrix, king_location):
+        color = self.to_move 
+        x, y = king_location[color]
+        # (alpha, beta) is the unit vector for the eight possible directions
+        for alpha, beta in [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]:
+            dx = [i * alpha for i in range(1, 8)]
+            dy = [i * beta for i in range(1, 8)]
+            for i in range(0, 7):
+                X, Y = x + dx[i], y + dy[i] 
+                # Case we X, Y not part of matrix
+                if not self.is_valid_matrix_location(X, Y):
+                    continue 
+                # Case where no piece in the current location
+                if matrix[X][Y] == None:
+                    continue
+                # Path guarded by some piece
+                if (matrix[X][Y].color == matrxi[x][y].color):
+                    break 
+                # Case when Opp Pawn
+                if matrix[X][Y].piece_type == "P":
+                    if (alpha != 0 and beta == 1) and (dx[i] == alpha and dy[i] == beta):
+                        return True 
+                    else:
+                        break
+                # Case when Opp King
+                if matrix[X][Y].piece_type == "K":
+                    if dx[i] == alpha and dy[i] == beta:
+                        return True
+                    else:
+                        break
+                # Opposite colored Queen in open path
+                if (matrix[X][Y].piece_type == "Q"):
+                    return True 
+                # Opposite colored Bishop in open path
+                if matrix[X][Y].piece_type == "B":
+                    if alpha != 0 and beta != 0:
+                        return True 
+                    else:
+                        break
+                # Opposite colored Rook in open path
+                if matrix[X][Y].piece_type == "R":
+                    if alpha == 0 or beta == 0:
+                        return True
+                    else:
+                        break
+        return False
+
+    def __is_check_state(self, matrix, king_location):
+        if self.__is_check_from_knight(matrix, king_location) or self.__is_row_column_diagonal_check(matrix, king_location):
+            return false
+    
+    def __king_check(self, move):
+        matrix, king_location = None, None
+        self.make_move(move, False, matrix, king_location)
+        if self.__is_check_state(matrix, king_location):
+            return False 
+        return True
 
     def initialize_board(self):
         self.matrix = [
@@ -183,7 +266,7 @@ class Board:
 
     def is_valid_move(self, move):
         ix, iy, fx, fy = move.initial_pos[0], move.initial_pos[1], move.final_pos[0], move.final_pos[1]
-        ipiece, fpiece = matrix[ix][iy], matrix[fx][fy]
+        ipiece, fpiece = self.matrix[ix][iy], self.matrix[fx][fy]
         # If piece is not present at the initial location
         if ipiece == None:
             return False
@@ -207,16 +290,41 @@ class Board:
             return False
         return True
 
-    def make_move(self, move):
+    def make_move(self, move, actual_move=True, matrix = None, king_location=None):
+        if not actual_move:
+            matrix = copy.deepcopy(self.matrix)
+        else:
+            matrix = self.matrix 
         ix, iy, fx, fy = move.initial_pos[0], move.initial_pos[1], move.final_pos[0], move.final_pos[1]
         ipiece, fpiece = matrix[ix][iy], matrix[fx][fy]
-        fpiece = copy.deepcopy(ipiece)
-        ipiece = None
-        self.print_board()
+        matrix[fx][fy], matrix[ix][iy] = copy.deepcopy(ipiece), None
+        # TODO: Handle the case when pawn moves to the last rank, and pawn promotion
+        if actual_move:
+            # If king move then update king location
+            if ipiece.piece_type == "K":
+                self.king_location[self.to_move] = [fx, fy]
+            # Update the color to move
+            self.to_move = 1 - self.to_move
+            # Print the board status
+            self.print_board()
+        else:
+            king_location = copy.deepcopy(self.king_location)
+            # If king move then update king location
+            if ipiece.piece_type == "K":
+                king_location[self.to_move] = [fx, fy]
 
     def check_state(self):
-        # TODO
-        pass
+        in_check = False 
+        if self.__is_check_state(self, self.matrix, self.king_location):
+            in_check = True
+        all_possible_moves = self.get_all_possible_moves()
+        if all_possible_moves == 0:
+            if in_check:
+                return "W"
+            else:
+                return "D"
+        else:
+            return "R"
 
     def print_board(self):
         for i in range(0, 8):
@@ -235,7 +343,7 @@ class Board:
         self.piece_list = ["P", "R", "B", "K", "Q", "K"]
 
         self.matrix = []
-        self.to_move = "W"
+        self.to_move = 0
         self.king_location = [[4, 0], [4, 7]]
 
         self.initialize_board()
